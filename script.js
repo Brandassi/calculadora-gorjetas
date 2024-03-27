5dbd651bf00b5d201aa9756cc6c56833ba7f410f
gustavo brandassi <gustavobrandassi@icloud.com>
2024-03-26 22:55:17 -0300

    quarto commit do js

--- script.js
+++ script.js
@@ -46,5 +46,15 @@ function receberPorcentagem(evento){
 
     })
 
-    porcentagem =parseFloat(evento.target.value)/100
+    if(evento.target.value !== "") {
+        porcentagem =parseFloat(evento.target.value)/100
+    }
+    else{
+        porcentagem = 0
+    }
+
+    
 }
+
+const gorjetaInput = document.querySelector("#outra")
+gorjetaInput.addEventListener("input", receberPorcentagem)
